300c891d734f4f180f808dcb10e313f7d3ceb325
gildardopatino <gaitancarlex@gmail.com>
2024-09-18 13:06:18 -0500

    Update

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,8 @@ const { Sequelize } = require('sequelize');
 
 let conexionBD;
 
-if (process.env.PRODUCTION) {
+if (process.env.PRODUCTION == 'true') {
+    console.log("a1ui1");
     conexionBD = new Sequelize(process.env.POSTGRES_URL, {
         dialect: 'postgres',
         dialectOptions: {
@@ -16,13 +17,7 @@ if (process.env.PRODUCTION) {
     conexionBD = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
-        dialect: 'postgres',
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
-        }
+        dialect: 'postgres'
     });
 }
 
