60bfc8969d2675ef43f367a9a1a0705ebfdd08cc
gildardopatino <gaitancarlex@gmail.com>
2024-09-18 12:33:19 -0500

    Actualizacion

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,15 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
+// Habilitamos a sequelize si el env PRODUCTION es true nos conectamos a la variable de ambiente de POSTGRES_URL si no, a las normales
+
+if (process.env.PRODUCTION) {
+    const conexionBD = new Sequelize(process.env.POSTGRES_URL, {
+        dialect: 'postgres',
+    });
+    module.exports = conexionBD;
+    return;
+}
 const conexionBD = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
