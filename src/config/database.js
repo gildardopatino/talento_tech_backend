e4a5b1457156cb648b4230cc00a0c53046e8730c
gildardopatino <gaitancarlex@gmail.com>
2024-09-18 12:40:38 -0500

    Update

--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,30 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-if (process.env.PRODUCTION) {
-    const conexionBD = new Sequelize(process.env.POSTGRES_URL, {
-        dialect: 'postgres',
-    });
-    module.exports = conexionBD;
-    return;
-}
-const conexionBD = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
+let conexionBD;
 
-});
+if (process.env.PRODUCTION) {
+    conexionBD = new Sequelize(process.env.POSTGRES_URL, {
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    });
+} else {
+    conexionBD = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    });
+}
 
 module.exports = conexionBD;
